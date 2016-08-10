c41590005a26d3a7b413a937c2e1532d2aaf63f9
Katagiri Takahiro <takahiro.katagiri@dena.com>
2016-08-10 14:53:00 +0900

    初期プロジェクトコミット

---
+++ Assets/Scripts/DestroyTest.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyTest : MonoBehaviour
+{
+	int loopCnt = 0;
+
+	public Event ev;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	void FixedUpdate()
+	{
+		Debug.Log ( "loopcnt fixed : " + loopCnt );
+
+		if (loopCnt >= 10)
+		{
+			Destroy(this.gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		loopCnt++;
+		Debug.Log ( "loopcnt : " + loopCnt );
+	}
+}
