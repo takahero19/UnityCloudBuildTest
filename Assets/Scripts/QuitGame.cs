343162aee1101150f0e9f55e198c17fbfc4417a8
Katagiri Takahiro <takahiro.katagiri@dena.com>
2016-08-10 15:41:30 +0900

    アプリ落とせるようにした

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuitGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnButtonClick()
+	{
+		Debug.Log ("Quit");
+
+		// ゲーム終了
+		Application.Quit ();
+	}
+}
